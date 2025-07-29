688ed11ceb5cf3ac76c178f2e593ec4522ad9c05
skywalker0803r <skywalker0803r@example.com>
2025-07-29 20:45:55 +0800

    調整週期 槓桿 策略 執行為單次 準備上線

--- vegas_run.py
+++ vegas_run.py
@@ -1,15 +1,15 @@
 from online.auto_trade_future import auto_trade_futures
-from Technicalindicatorstrategy import testsma
+from Technicalindicatorstrategy import vegas
 
 auto_trade_futures(
     symbol="ETH/USDT",
-    interval="1m",
+    interval="1h",
     usdt_percent_per_order=0.1,  # 10%
-    leverage=3,
-    strategy=testsma,
+    leverage=10,
+    strategy=vegas,
     stop_loss=0.005,
     take_profit=0.05,
     max_hold_bars=1000,
-    run_once=False
+    run_once=True
 )
 
