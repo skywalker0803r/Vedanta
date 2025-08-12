d432511326c0ddae788649cb46f76913f0b867ec
skywalker0803r <skywalker0803r@gmail.com>
2025-08-12 13:54:27 +0800

    合約測試網一次跑十種幣 用海龜策略

--- turtle_strategy_run.py
+++ turtle_strategy_run.py
@@ -1,6 +1,7 @@
 from online.auto_trade_future import auto_trade_futures
 from Technicalindicatorstrategy import turtle_strategy
 
+# 1
 auto_trade_futures(
     symbol="ETH/USDT",#幣種
     interval="1h",#週期
@@ -13,4 +14,119 @@ auto_trade_futures(
     run_once=True #單次執行模式
 )
 
+# 2
+auto_trade_futures(
+    symbol="BTC/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
 
+# 3
+auto_trade_futures(
+    symbol="XRP/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 4
+auto_trade_futures(
+    symbol="BNB/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 5
+auto_trade_futures(
+    symbol="SOL/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 6
+auto_trade_futures(
+    symbol="DOGE/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 7
+auto_trade_futures(
+    symbol="ADA/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 8
+auto_trade_futures(
+    symbol="SUI/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 9
+auto_trade_futures(
+    symbol="XLM/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
+
+# 10
+auto_trade_futures(
+    symbol="HYPE/USDT",#幣種
+    interval="1h",#週期
+    usdt_percent_per_order=1,# 每次使用的資金佔比
+    leverage=1,#槓桿
+    strategy=turtle_strategy,# 策略
+    stop_loss=None,# 停損閾值
+    take_profit=None, # 停利閾值
+    max_hold_bars=100000,# 最大持有K棒數
+    run_once=True #單次執行模式
+)
