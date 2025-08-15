de67025ae1c7c07642bdfabd0b15c2049519ca10
skywalker0803r <skywalker0803r@gmail.com>
2025-08-15 23:14:58 +0800

    改成一小時回測結果 兩個TG機器人改成每小時週期 幣安測試網策略也改成每小時

--- turtle_run.py
+++ turtle_run.py
@@ -4,12 +4,12 @@ from Technicalindicatorstrategy import turtle_strategy
 # 1
 auto_trade_futures(
     symbol="ETH/USDT",#幣種
-    interval="4h",#週期
+    interval="1h",#週期
     usdt_percent_per_order=0.1,# 每次使用的資金佔比
-    leverage=10,#槓桿
+    leverage=1,#槓桿
     strategy=turtle_strategy,# 策略
-    stop_loss = 0.01,       # 停損閾值，例如0.05代表5%
-    take_profit = 0.17,     # 停利閾值
+    stop_loss = 0.05,       # 停損閾值，例如0.05代表5%
+    take_profit = 0.18,     # 停利閾值
     max_hold_bars=100000,# 最大持有K棒數
     run_once=True #單次執行模式
 )
@@ -17,12 +17,12 @@ auto_trade_futures(
 # 2
 auto_trade_futures(
     symbol="BTC/USDT",#幣種
-    interval="4h",#週期
+    interval="1h",#週期
     usdt_percent_per_order=0.1,# 每次使用的資金佔比
-    leverage=10,#槓桿
+    leverage=1,#槓桿
     strategy=turtle_strategy,# 策略
-    stop_loss = 0.01,       # 停損閾值，例如0.05代表5%
-    take_profit = 0.17,     # 停利閾值
+    stop_loss = 0.05,       # 停損閾值，例如0.05代表5%
+    take_profit = 0.18,     # 停利閾值
     max_hold_bars=100000,# 最大持有K棒數
     run_once=True #單次執行模式
 )
@@ -30,12 +30,12 @@ auto_trade_futures(
 # 3
 auto_trade_futures(
     symbol="XRP/USDT",#幣種
-    interval="4h",#週期
+    interval="1h",#週期
     usdt_percent_per_order=0.1,# 每次使用的資金佔比
-    leverage=10,#槓桿
+    leverage=1,#槓桿
     strategy=turtle_strategy,# 策略
-    stop_loss = 0.01,       # 停損閾值，例如0.05代表5%
-    take_profit = 0.17,     # 停利閾值
+    stop_loss = 0.05,       # 停損閾值，例如0.05代表5%
+    take_profit = 0.18,     # 停利閾值
     max_hold_bars=100000,# 最大持有K棒數
     run_once=True #單次執行模式
 )
